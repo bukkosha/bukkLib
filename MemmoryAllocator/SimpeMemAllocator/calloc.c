abb79b7e6b86f05c6041d539e7cb9948f98560fd
bukkosha <mak.kurlovich@gmail.com>
2024-07-04 22:22:53 +0700

    Calloc func

---
+++ MemmoryAllocator/SimpeMemAllocator/calloc.c
@@ -0,0 +1,17 @@
+#include "Allocator.h"
+
+void *mycalloc(size_t nmemb, size_t size) {
+    if(size < 1 || nmemb < 1)
+        return NULL;
+
+    size_t total_size = size * nmemb;
+    if(size != total_size / nmemb)
+        return NULL;
+
+    void *block = mymalloc(total_size);
+    if(!block)
+        return NULL;
+
+    memset(block, 0, size);
+    return block;
+}
